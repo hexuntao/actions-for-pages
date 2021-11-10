71461828ebbf64bcba88012042adfe29c7f985b5
hexuntao <hexuntao@foxmail.com>
2021-11-10 11:57:44 +0800

    test

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import { useSpring, animated } from 'react-spring';
 function App() {
   const style = useSpring({
     loop: { reverse: true },
-    from: { x: 0 },
-    to: { x: 100 }
+    from: { x: 100 },
+    to: { x: 500 }
   });
-  console.log(style);
   return (
     <div className="App">
       <div style={{ position: 'absolute' }}>
         <animated.div
           style={{
-            width: 80,
-            height: 80,
+            width: 100,
+            height: 100,
             backgroundColor: '#46e891',
-            borderRadius: 16,
+            borderRadius: 20,
             ...style
           }}
         ></animated.div>
